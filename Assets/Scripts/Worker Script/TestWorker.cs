4dd5da4efe4f6da0634ab5a39de049e02209db0a
RexRegius2004 <134357506+RexRegius2004@users.noreply.github.com>
2026-06-10 15:16:28 +0800

    Debug Panel & Worker Rarity

--- Assets/Scripts/Worker Script/TestWorker.cs
+++ Assets/Scripts/Worker Script/TestWorker.cs
@@ -2,14 +2,54 @@ using UnityEngine;
 
 public class TestWorker : MonoBehaviour
 {
-    public WorkerData testWorker;
+    public WorkerData common;
+    public WorkerData uncommon;
+    public WorkerData rare;
+    public WorkerData epic;
+    public WorkerData Legendary;
     public WorkerManager workerManager;
 
-void Update()
-{
-    if (Input.GetKeyDown(KeyCode.Space))
+    public GameObject debugPanel;
+
+    void Start()
     {
-        workerManager.HireWorker(testWorker);
+        CloseDebugPanel();
     }
-}
+    void Update()
+    {
+    
+    }
+
+    public void OpenDebugPanel()
+    {
+        debugPanel.SetActive(true);
+    }
+
+    public void CloseDebugPanel()
+    {
+        debugPanel.SetActive(false);
+    }
+    public void AddCommonWorker()
+    {
+    workerManager.HireWorker(common);
+    }
+
+    public void AddUncommonWorker()
+    {
+    workerManager.HireWorker(uncommon);
+    }
+    public void AddRareWorker()
+    {
+    workerManager.HireWorker(rare);
+    }
+
+    public void AddEpicWorker()
+    {
+    workerManager.HireWorker(epic);
+    }
+
+    public void AddLegendaryWorker()
+    {
+    workerManager.HireWorker(Legendary);
+    }
 }
