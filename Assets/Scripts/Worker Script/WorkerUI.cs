a6e4a501107114e3aec658f04ca9cf2b896c34ee
RexRegius2004 <134357506+RexRegius2004@users.noreply.github.com>
2026-06-23 18:03:33 +0800

    burnout, recovery, and success rate mechanics

--- Assets/Scripts/Worker Script/WorkerUI.cs
+++ Assets/Scripts/Worker Script/WorkerUI.cs
@@ -1,18 +1,30 @@
 using UnityEngine;
-using System.Collections.Generic;
 using UnityEngine.UI;
 using TMPro;
+
 public class WorkerUI : MonoBehaviour
 {
+    [Header("UI References")]
     public Image icon;
     public TextMeshProUGUI progressText;
+
+    [Header("Sliders")]
+    public Slider progressSlider;
+    public Slider burnoutSlider;
+
+    [Header("Colors")]
+    public Color normalColor = Color.white;
+    public Color burnedOutColor = Color.red;
 
     private WorkerInstance worker;
 
     public WorkerUI Bind(WorkerInstance worker)
     {
         this.worker = worker;
-        icon.sprite = worker.data.icon;
+
+        if (worker.data.icon != null)
+            icon.sprite = worker.data.icon;
+
         return this;
     }
 
@@ -20,7 +32,28 @@ public class WorkerUI : MonoBehaviour
     {
         if (worker == null) return;
 
-        float percent = worker.currentProgress / worker.data.documentLength;
-        progressText.text = Mathf.RoundToInt(percent * 100f) + "%";
+        float progress = Mathf.Clamp01(worker.GetProgressPercent());
+        float burnout = Mathf.Clamp01(worker.GetBurnoutPercent());
+
+        bool burnedOut = worker.IsBurnedOut();
+
+        // 🧠 TEXT
+        if (burnedOut)
+        {
+            progressText.text = "Burned Out";
+            progressText.color = burnedOutColor;
+        }
+        else
+        {
+            progressText.text = Mathf.RoundToInt(progress * 100f) + "%";
+            progressText.color = normalColor;
+        }
+
+        // 📊 SLIDERS
+        if (progressSlider != null)
+            progressSlider.value = progress;
+
+        if (burnoutSlider != null)
+            burnoutSlider.value = burnout;
     }
 }
